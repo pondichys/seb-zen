596b872322f4ebdffb11749a3d6cefc5fcb9e9fc
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2023-10-24 08:54:18 +0200

    feat(picom): add blur and disable rounded corner
    Requires picom-allusive from AUR
    Also requires mesa and vulkan drivers for your GPU
    picom needs to be started with --experimental-backends flag

--- scripts/picom-toggle.sh
+++ scripts/picom-toggle.sh
@@ -3,5 +3,6 @@ if pgrep -x "picom" > /dev/null
 then
 	killall picom
 else
-	picom -b --config ~/.config/leftwm/themes/current/picom.conf
+	# picom -b --config ~/.config/leftwm/themes/current/picom.conf
+  picom --config "$HOME/.config/leftwm/themes/current/picom.conf" -b --animations --animation-window-mass 0.5 --experimental-backends  &> /dev/null &
 fi
